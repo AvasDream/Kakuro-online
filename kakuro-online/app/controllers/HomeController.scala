5f91d185ace6284f1737852530f22f17bfe61e98
MisterShoh <herr_shoh@mail.ru>
2018-11-13 22:46:27 +0100

    CSS Datei erweitert

--- kakuro-online/app/controllers/HomeController.scala
+++ kakuro-online/app/controllers/HomeController.scala
@@ -1,29 +1,24 @@
 package controllers
 
 import javax.inject._
-import javax.swing.plaf.TextUI
+import play.api._
+import play.api.mvc._
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+    * Create an Action to render an HTML page.
+    *
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
-  }
-
-  def kakuro(command: String) : Result = {
-    tui: TextUI = Kakuro.getInstance().getTui()
-    tui.processInputLine(command)
-    return ok(sudoku.render(tui.toHtml()))
   }
 }
