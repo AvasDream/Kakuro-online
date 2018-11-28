9508e3004792da25c5b6ac222f046c8dc99ae0fa
Vincent Grimmeisen <vincegrixc@gmail.com>
2018-11-28 14:32:07 +0100

    Added new function controller.getField:fieldInterface to native kakuro project. Created and added new Jar file

--- kakuro-online/app/controllers/KakuroController.scala
+++ kakuro-online/app/controllers/KakuroController.scala
@@ -9,7 +9,8 @@ import com.google.inject.Guice
 import de.htwg.se.kakuro.controller.controllerComponent.ControllerInterface
 import de.htwg.se.kakuro.KakuroModule
 import de.htwg.se.kakuro.aview.Tui
-
+import de.htwg.se.kakuro.model.fieldComponent
+import play.api.Logger
 /**
   * This controller creates an `Action` to handle HTTP requests to the
   * application's home page.
@@ -17,11 +18,15 @@ import de.htwg.se.kakuro.aview.Tui
 @Singleton
 class KakuroController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-
+  // Best practice logging
+  // val logger: Logger = Logger(this.getClass())
   val injector = Guice.createInjector(new KakuroModule)
   val controller = injector.getInstance(classOf[ControllerInterface])
-  val tui = new Tui(controller)
-  val string = tui.toString()
+  controller.initField
+  controller.set(1,1,3)
+  Logger.info("Hello World")
+  val field = controller.getField
+  val string = controller.fieldToString
   def kakuro() = Action {implicit request: Request[AnyContent] =>
     Ok(views.html.test(string))
   }
