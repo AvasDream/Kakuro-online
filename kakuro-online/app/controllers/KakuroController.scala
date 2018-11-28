0cbb33a8c4c8484e7f74cee4cb18b4fdbe76fd46
Vincent Grimmeisen <vincegrixc@gmail.com>
2018-11-28 16:33:49 +0100

    Architecture use jquery and ajax to transfer and fill field. Implemented helper class in Kakurocontroller to convert to json

--- kakuro-online/app/controllers/KakuroController.scala
+++ kakuro-online/app/controllers/KakuroController.scala
@@ -1,16 +1,15 @@
 package controllers
 
 import javax.inject._
-import play.api._
+
 import play.api.mvc._
-import de.htwg.se.kakuro.Kakuro._
-import de.htwg.se.kakuro.model.fieldComponent.FieldImpl.{ Field, FieldCreator }
+import de.htwg.se.kakuro.model.fileIOComponent.fileIOJsonImpl
 import com.google.inject.Guice
 import de.htwg.se.kakuro.controller.controllerComponent.ControllerInterface
 import de.htwg.se.kakuro.KakuroModule
-import de.htwg.se.kakuro.aview.Tui
-import de.htwg.se.kakuro.model.fieldComponent
+import de.htwg.se.kakuro.model.fieldComponent.{CellInterface, FieldInterface}
 import play.api.Logger
+import play.api.libs.json.{JsNumber, JsObject, Json, Writes}
 /**
   * This controller creates an `Action` to handle HTTP requests to the
   * application's home page.
@@ -29,8 +28,48 @@ class KakuroController @Inject()(cc: ControllerComponents) extends AbstractContr
   val height = field.height
   val width = field.width
 
-
+  val helper = new helper
+  val json = helper.gridToJson(field);
   def kakuro() = Action {implicit request: Request[AnyContent] =>
     Ok(views.html.test(field))
+  }
+
+
+
+}
+
+
+class helper() {
+
+  def gridToJson(grid: FieldInterface): JsObject = {
+    Json.obj(
+      "grid" -> Json.obj(
+        "width" -> JsNumber(grid.width),
+        "height" -> JsNumber(grid.height),
+        "cells" -> Json.toJson(
+          for {
+            row <- 0 until grid.width
+            col <- 0 until grid.height
+          } yield {
+            Json.obj(
+              "row" -> row,
+              "col" -> col,
+              "cell" -> Json.toJson(grid.cell(row, col))
+            )
+          }
+        )
+      )
+    )
+  }
+  implicit val cellWrites: Writes[CellInterface] {
+    def writes(cell: CellInterface): JsObject
+  } = new Writes[CellInterface] {
+
+    def writes(cell: CellInterface): JsObject = Json.obj(
+      "value" -> cell.value,
+      "right" -> cell.rightSum,
+      "down" -> cell.downSum,
+      "type" -> { if (cell.isWhite) 1 else if (cell.isBlack) 2 else 0 }
+    )
   }
 }
